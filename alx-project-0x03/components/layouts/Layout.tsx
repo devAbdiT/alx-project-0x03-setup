8953018ffe9796812cdabbbc9a036f7f32bdf461
Abdi begna <abditadesse89@gmail.com>
2025-11-24 00:04:14 +0300

    error correction

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,23 +1,3 @@
-// import { ReactNode } from "react";
-// import Footer from "./Footer";
-// import Header from "./Header";
-// import { LayoutProps } from "@/interface";
-// interface LayoutProps {
-//   children: ReactNode;
-// }
-
-// const Layout: React.FC<LayoutProps> = ({ children }) => {
-//   return (
-//     <>
-//       <Header />
-//       <main>{children}</main>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Layout;
-
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
