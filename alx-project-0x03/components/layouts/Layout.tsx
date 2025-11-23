133977715eec3941157cf8857cb34f345dc81e36
Abdi begna <abditadesse89@gmail.com>
2025-11-23 23:58:35 +0300

    Restructring the code with interface

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,10 +1,27 @@
+// import { ReactNode } from "react";
+// import Footer from "./Footer";
+// import Header from "./Header";
+// import { LayoutProps } from "@/interface";
+// interface LayoutProps {
+//   children: ReactNode;
+// }
+
+// const Layout: React.FC<LayoutProps> = ({ children }) => {
+//   return (
+//     <>
+//       <Header />
+//       <main>{children}</main>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Layout;
+
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import { LayoutProps } from "@/interface";
-interface LayoutProps {
-  children: ReactNode;
-}
+import { LayoutProps } from "@/interfaces";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
